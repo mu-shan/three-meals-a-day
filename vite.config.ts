4b04810d9a66590098c7f05cfba11ff352661cd8
木杉 <1715722187@qq.com>
2026-07-10 17:19:57 +0800

    ci: deploy app to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+
 export default defineConfig({
+  base: repositoryName ? `/${repositoryName}/` : '/',
   plugins: [vue()],
   test: {
     environment: 'node',
